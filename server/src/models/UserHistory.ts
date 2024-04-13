6c8e63f35f3c2df87863506f7b14c4fba6c15498
AlexCoding <alexcoding10@gmail.com>
2024-04-13 19:26:51 +0200

    modularizacion de enum y creacion de UserHistory

---
+++ server/src/models/UserHistory.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Document, Types } from "mongoose";
+import{ StatusNote, StatusUser } from "./enums";
+
+
+interface UserHistory extends Document {
+    userId: Types.ObjectId; // Referencia al ID del usuario original
+    changes: {
+        name?: string;
+        email?: string;
+        profileImage?: string;
+        description?: string;
+        friends?: Types.ObjectId[];
+        status?: StatusUser;
+        updateAt: Date;
+    }[];
+}
+
+const userHistorySchema = new Schema<UserHistory>({
+    userId: { type: Types.ObjectId, ref: "User" }, // Referencia al ID del usuario original
+    changes: [{
+        name: String,
+        email: String,
+        profileImage: String,
+        description: String,
+        friends: [{ type: Types.ObjectId, ref: "User" }],
+        status: { type: String, enum: Object.values(StatusUser) },
+        updateAt: { type: Date, default: Date.now }
+    }]
+});
+
+const UserHistoryModel = model<UserHistory>("UserHistory", userHistorySchema);
+
+export default UserHistoryModel;
